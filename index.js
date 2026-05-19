229ffa52fac29288117fe05090d197fed0162e42
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-05-19 09:54:36 +0600

    Update .gitignore and enhance MongoDB connection setup in index.js

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 const port = process.env.PORT || 4000
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+
+
+    const db =client.db('pawly')
+    const petsCollection = db.collection('pets')
+
+    
+
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
