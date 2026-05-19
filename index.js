e399efc92fbf8c90a102f29008b59c76e581d6c8
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-05-19 12:25:48 +0600

    get single pet api create

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-dotenv.config()
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -17,31 +17,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
 
+    const db = client.db("pawly");
+    const petsCollection = db.collection("pets");
 
+    // pets apis
+    app.post("/add-pet", async (req, res) => {
+      const pet = req.body;
+      const result = await petsCollection.insertOne(pet);
+      res.send(result);
+    });
 
-    const db =client.db('pawly')
-    const petsCollection = db.collection('pets')
+    app.get("/pets", async (req, res) => {
+      const cursor = await petsCollection.find().toArray();
+      res.send(cursor);
+    });
 
-    app.post('/add-pet', async (req, res) => {
-      const pet = req.body
-      const result = await petsCollection.insertOne(pet)
-      res.send(result)
-    })
-
-    app.get('/pets', async (req, res) => {
-      const cursor = petsCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-
+    app.get("/pets/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const pet = await petsCollection.findOne(query);
+      res.send(pet);
+    });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -50,13 +54,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
